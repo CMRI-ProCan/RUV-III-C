37923989c212e33ad1073a59ebd7c876fa5f566e
Rohan Shah <rshah@cmri.org.au>
2019-10-18 12:00:51 +1100

    Minor changes

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Peptide data from the cross-lab study of Collins et al (2017)
 #'
-#' A dataset containing the peptide-level data from the cross-lab study of Collins et al (2017), originally published in Nature Communications. The dataconsists of a data.frame named \code{peptideData}, and a character vector sisPeptides, listing the peptides that should be analysed. 
+#' A dataset containing the peptide-level data from the cross-lab study of Collins et al (2017), originally published in Nature Communications. The data consists of a data.frame named \code{peptideData}, and a character vector sisPeptides, listing the peptides that should be analysed. 
 #' @format A data.frame containing peptide-level data, with peptides that were not detected coded as NA. All columns are peptides, except for \code{filename}, \code{site}, \code{mixture}, \code{grouping} and \code{Date}.
 "peptideData"
 
